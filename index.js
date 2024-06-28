875677d54eba8aa2348247139e05273954474226
mushfiqbh <mushfiqbh@gmail.com>
2024-06-28 16:19:09 +0600

    First Commit 3

--- index.js
+++ index.js
@@ -15,27 +15,30 @@ expressApp.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 bot.command("start", (ctx) => {
-  console.log(ctx.from);
+  console.log(ctx.from.id);
   bot.telegram.sendMessage(
     ctx.chat.id,
-    "Hello there! Welcome to the Code Capsules telegram bot.\nI respond to /ethereum. Please try it",
+    "Hello there! Welcome to the Code Capsules telegram bot.\nI respond to /ethereum. Please try it m",
     {}
   );
 });
 
 bot.command("ethereum", (ctx) => {
   var rate;
-  console.log(ctx.from);
   axios
     .get(
       `https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd`
     )
     .then((response) => {
-      console.log(response.data);
       rate = response.data.ethereum;
       const message = `Hello, today the ethereum price is ${rate.usd}USD`;
       bot.telegram.sendMessage(ctx.chat.id, message, {});
     });
 });
 
+// expressApp.use(bot.webhookCallback('/secret-path'))
+// bot.telegram.setWebhook('<YOUR_CAPSULE_URL>/secret-path')
+
 bot.launch();
+
+// expressApp.listen(port, () => console.log(`Listening on ${port}`));
